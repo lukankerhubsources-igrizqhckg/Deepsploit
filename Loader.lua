ef0c5023c6f3cbb674c50c0267813b860604b63f
lukankerhub <126632323+lukankerhub@users.noreply.github.com>
2023-03-01 02:44:40 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lukankerhub/Deepsploit/main/Prototype.lua?token=GHSAT0AAAAAAB7NC4CSQXWGLYJVSXMILJQCY76VSPQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lukankerhub/Deepsploit/main/Prototype.lua"))()
 
 game.Players.LocalPlayer.CharacterAdded:Connect(function()
     wait(3)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/lukankerhub/Deepsploit/main/Prototype.lua?token=GHSAT0AAAAAAB7NC4CSQXWGLYJVSXMILJQCY76VSPQ"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lukankerhub/Deepsploit/main/Prototype.lua"))()
 end)
