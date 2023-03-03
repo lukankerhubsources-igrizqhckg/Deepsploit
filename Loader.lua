9b6ebd940abced3eb195c2449d25746039783a4c
lukankerhubsources-igrizqhckg <126632323+lukankerhubsources-igrizqhckg@users.noreply.github.com>
2023-03-03 18:05:20 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lukankerhubsources/Deepsploit/main/Prototype.lua"))()
+local GitHubName = loadstring(game:HttpGet("https://pastebin.com/raw/sS94Uwjg"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/" + GitHubName + "/Deepsploit/main/Prototype.lua"))()
 
 game.Players.LocalPlayer.CharacterAdded:Connect(function()
     wait(3)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/lukankerhubsources/Deepsploit/main/Prototype.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/" + GitHubName + "/Deepsploit/main/Prototype.lua"))()
 end)
